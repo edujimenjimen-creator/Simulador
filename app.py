0ffe2e88420c122267a76b78357c869e888e263a
edujimenjimen-creator <edujimenjimen@gmail.com>
2026-09-17 19:44:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 # ==========================================
 # 0. CONFIGURACIÓN DE PÁGINA STREAMLIT
 # ==========================================
-st.set_page_config(page_title="OPM Guadix - Planificación Semanal", layout="wide")
+st.set_page_config(page_title="Simulador semanal", layout="wide")
 st.title("🏭 OPM Guadix: Panel de Planificación Semanal Optimizada")
 
 # ==========================================
